c38806afc9baa3462a9e85ac6da0bdba5502f43a
gouzal <larbi-inco@live.fr>
2018-08-30 15:29:12 +0100

    add properties to Article's migration file

--- database/migrations/2018_08_30_142232_create_articles_table.php
+++ database/migrations/2018_08_30_142232_create_articles_table.php
@@ -4,17 +4,18 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
-{
+class CreateArticlesTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -24,8 +25,8 @@ class CreateArticlesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('articles');
     }
+
 }
