51cb7dc7a6fd0eca79aa8a7780ef768fe3e7cb87
gouzal <larbi-inco@live.fr>
2018-10-15 11:21:47 +0100

    add routes api

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('articles', 'ArticleController@index');
+//Route::get('articles/{article}', 'ArticleController@show');
+//Route::post('articles', 'ArticleController@store');
+//Route::put('articles/{article}', 'ArticleController@update');
+//Route::delete('articles/{article}', 'ArticleController@delete');
