70d2e1a4e7164b06149572404055d44eed9c6e33
gouzal <larbi-inco@live.fr>
2018-10-17 17:03:01 +0100

    ...

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use App\Http\Resources\ArticleResource;
+use Validator;
 
 class ArticleController extends Controller {
 
@@ -22,15 +23,44 @@ class ArticleController extends Controller {
     }
 
     public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+                    'title' => 'required|min:6',
+                    'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["data" => null, "message" => $validator->errors()], 422);
+        }
         $article = Article::create($request->all());
 
-        return response()->json($article, 201);
+        if ($article) {
+            return response()->json(["data" => new ArticleResource($article), "message" => "article created."], 201);
+        }
+
+        return response(["data" => null, "message" => "can not create an Article"], 522);
     }
 
-    public function update(Request $request, Article $article) {
-        $article->update($request->all());
-
-        return response()->json($article, 200);
+    public function update(Request $request,Article $article) {
+//        $validator = Validator::make($request->all(), [
+//                    'title' => 'required|min:6',
+//                    'body' => 'required',
+//        ]);
+//         if ($validator->fails()) {
+//            return response()->json(["data" => null, "message" => $validator->errors()], 422);
+//        }
+//        if(!$article){
+//            return response()->json(['message' => 'Not Found!'], 404);
+//        }
+//        $article->update($request->all());
+//       if ($article) {
+//            return response()->json(["data" => new ArticleResource($article), "message" => "article updated."], 201);
+//        }
+//        return response()->json(['message' => 'Not Found!'], 404);
+        
+//        if(!$request->has('title') && $request->get("title")==''){
+//            return response()->json(["data" => null, "message" => "title is required!"], 422);
+//        }
+//        return "hh";
     }
 
     public function delete(Article $article) {
